5b8c9b72e62c7f29ead38ce9350a6281db26d18e
Thanh Nguyen <spiderpthanh@gmail.com>
2019-03-01 09:27:56 -0500

    work on level 2

--- Level 2 Project/java/java.js
+++ Level 2 Project/java/java.js
@@ -1,21 +1,58 @@
 function initialize()
 {
+	currentTurn = "player";
 	amountPlayerRedTokens = 12;
 	amountPlayerBlackTokens = 12;
 	amountComputerRedTokens = 12;
 	amountComputerBlackTokens = 12;
-	redDiceValue = 0;
-	blackDiceValue = 0;
+	playerRedDiceValue = 0;
+	playerBlackDiceValue = 0; 
+	computerRedDiceValue = 0;
+	computerBlackDiceValue = 0;
+	currentEventLog = "";
 	redDieFaceOutput = document.getElementById("redDieFace");
 	blackDieFaceOutput = document.getElementById("blackDieFace");
-	computerTokenOutput = document.getElementById("computerTokenDisplay")
-	playerTokenOutput = document.getElementById("playerTokenDisplay")
-	eventLogOutput = document.getElementById("eventLog")
+	computerTokenOutput = document.getElementById("computerTokenDisplay");
+	playerTokenOutput = document.getElementById("playerTokenDisplay");
+	eventLogOutput = document.getElementById("eventLog");
+	playerLastRedRollOutput = document.getElementById("playerLastRedRoll");
+	playerLastBlackRollOutput = document.getElementById("playerLastBlackRoll");
+	computerLastRedRollOutput = document.getElementById("computerLastRedRoll");
+	computerLastBlackRollOutput = document.getElementById("computerLastBlackRoll");
+	// MAKE DIFFERENT DICE FOR PLAYER AND COMPUTER
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 	
 }
 function rollDice()
 {
-	redDiceValue = getRandomInteger(1,6);
-	blackDiceValue = getRandomInteger(1,6);
+	if (currentTurn == "player") {
+		playerRedDiceValue = getRandomInteger(1,6);
+		playerBlackDiceValue = getRandomInteger(1,6);
+	}
+	if (currentTurn == "computer") {
+		computerRedDiceValue = getRandomInteger(1,6);
+		computerBlackDiceValue = getRandomInteger(1,6);
+	}
 	display();
 }
 function getRandomInteger(lower, upper)
@@ -27,25 +64,30 @@ function getRandomInteger(lower, upper)
 function changeTokens(functionNumber)
 {
 	if (functionNumber == 1) {
-		amountPlayerRedTokens += redDiceValue;
-		amountComputerBlackTokens -= blackDiceValue;
-		display(added);
+		amountPlayerRedTokens += playerRedDiceValue;
+		amountComputerBlackTokens -= computerBlackDiceValue;
+		concatenateEventLog("added to", "red", "removed from", "black");
 	}
 	if (functionNumber == 2) {
-		amountPlayerBlackTokens += blackDiceValue;
-		amountComputerRedTokens -= redDiceValue;
-		display();
+		amountPlayerBlackTokens += playerBlackDiceValue;
+		amountComputerRedTokens -= computerRedDiceValue;
+		concatenateEventLog("added to", "black", "removed from", "red");
 	}
 	if (functionNumber == 3) {
-		amountPlayerRedTokens -= redDiceValue;
-		amountComputerBlackTokens += blackDiceValue;
-		display();
+		amountPlayerRedTokens -= playerRedDiceValue;
+		amountComputerBlackTokens += computerBlackDiceValue;
+		concatenateEventLog("removed from", "red", "added to", "black");
 	}
 	if (functionNumber == 4) {
-		amountPlayerBlackTokens -= blackDiceValue;
-		amountComputerRedTokens += redDiceValue;
-		display();
+		amountPlayerBlackTokens -= playerBlackDiceValue;
+		amountComputerRedTokens += computerRedDiceValue;
+		concatenateEventLog("removed from", "black", "added to", "red");
 	}
+}
+function concatenateEventLog(playerPile, playerColor, computerPile, computerColor)
+{
+	currentEventLog = "<br>" + playerRedDiceValue + " tokens " + playerPile + " your " + playerColor + " pile and " + playerBlackDiceValue + " tokens " + computerPile + " the computer's " + computerColor + " pile.";
+	display();
 }
 function reset()
 {
@@ -53,12 +95,19 @@ function reset()
 	blackDieFaceOutput.src = "images/black-0.png";
 	redDiceValue = 0;
 	blackDiceValue = 0;
+	computerTokenOutput.innerHTML = "";
+	playerTokenOutput.innerHTML = "";
+	eventLogOutput.innerHTML += "";
 }
-function display(modifyPlayer, )
+function display()
 {
-	redDieFaceOutput.src = "images/red-" + redDiceValue + ".png";
-	blackDieFaceOutput.src = "images/black-" + blackDiceValue + ".png";
+	redDieFaceOutput.src = "images/red-" + playerRedDiceValue + ".png";
+	blackDieFaceOutput.src = "images/black-" + playerBlackDiceValue + ".png";
 	computerTokenOutput.innerHTML = "The Computer has " + amountComputerRedTokens + " red tokens and " + amountComputerBlackTokens + " black tokens."
 	playerTokenOutput.innerHTML = "You have " + amountPlayerRedTokens + " red tokens and " + amountPlayerBlackTokens + " black tokens."
-	eventLogOutput.innerHTML += "<br>" + redDiceValue + " was"
+	playerLastRedRollOutput.innerHTML = playerRedDiceValue;
+	playerLastBlackRollOutput.innerHTML = playerBlackDiceValue;
+	computerLastRedRollOutput.innerHTML = computerRedDiceValue;
+	computerLastBlackRollOutput.innerHTML = computerBlackDiceValue;
+	eventLogOutput.innerHTML += currentEventLog;
 }
