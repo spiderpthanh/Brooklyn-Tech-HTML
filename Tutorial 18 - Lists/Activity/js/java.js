12268ceb9fc09db20d95c80ea9bc02972ac13467
Thanh Nguyen <spiderpthanh@gmail.com>
2019-04-16 09:27:53 -0400

    work on 17 and 18

--- Tutorial 18 - Lists/Activity/js/java.js
+++ Tutorial 18 - Lists/Activity/js/java.js
@@ -1,4 +1,13 @@
-function initialize
+function initialize()
 {
- thanh sucks penis
+	titleOutput = document.getElementById("title")
+	item1Output = document.getElementById("item1")
+	item2Output = document.getElementById("item2")
+	item3Output = document.getElementById("item3")
+	item4Output = document.getElementById("item4")
+	item5Output = document.getElementById("item5")
+}
+function swapText(mod)
+{
+	
 }
